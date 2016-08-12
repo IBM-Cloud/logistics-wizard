88e42208afcaa6b837fd26025d77de25526bc496
COLBY C. WILLIAMS <colbycheeze@us.ibm.com>
2016-08-12 16:15:15 -0500

    Started visual component for CreateDemo flow

--- client-src/src/routes/CreateDemo/components/CreateDemo.jsx
+++ client-src/src/routes/CreateDemo/components/CreateDemo.jsx
@@ -5,21 +5,40 @@ import RaisedButton from 'material-ui/RaisedButton';
 import classes from './CreateDemo.scss';
 
 const styles = {
-  padding: '1rem',
+  container: {
+    display: 'flex',
+    alignItems: 'stretch',
+  },
+  button: {
+    float: 'right',
+    marginTop: '2rem',
+  },
+  fields: {
+    width: '100%',
+  },
 };
 
 export const CreateDemo = (props) => (
-  <Paper style={styles} rounded>
-    <p>Give your project a name (Optional)</p>
-    <TextField hintText="Supply Chain Logistics" />
-    <p>Enter your email address (Optional)</p>
-    <TextField hintText="UserName@business.com" />
-    <br />
-    <RaisedButton label="LET'S GO" />
+  <Paper style={styles.container} rounded>
+    <div className={classes.leftBar} />
+    <div className={classes.infoSection}>
+      <h2>Create Demo Session</h2>
+      <p>We promise to never bug you. While not necessary, providing us with your email address gives us an easy way to send you an email with your unique session ID.</p>
+      <p className="thin">Already created a project? You can access a previous created session using the link sent to the email provided to us.</p>
+    </div>
+    <div className={classes.mainSection}>
+      <p>Give your project a name</p>
+      <TextField style={styles.fields} hintText="Supply Chain Logistics" />
+      <p>Enter your email address (Optional)</p>
+      <TextField style={styles.fields} hintText="UserName@business.com" />
+      <br />
+      <RaisedButton style={styles.button} label="LET'S GO" onClick={props.createDemo} />
+    </div>
   </Paper>
 );
 
 CreateDemo.propTypes = {
+  createDemo: React.PropTypes.func.isRequired,
 };
 
 export default CreateDemo;
--- client-src/src/routes/Home/components/HomeView.jsx
+++ client-src/src/routes/Home/components/HomeView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './HomeView.scss';
+import CreateDemo from 'routes/CreateDemo/components/CreateDemo';
 
 export const HomeView = () => (
   <div className={classes.homeView}>
     <h1>Logistics Wizard</h1>
     <h4>TODO: Build "landing page" here</h4>
+    <CreateDemo />
   </div>
 );
 
