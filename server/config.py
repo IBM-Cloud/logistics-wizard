5f8d331e291bd4a751ccbb1d9919f52319c9d9fa
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2016-09-07 09:46:11 +0200

    movedmoved controller to its repo https://github.com/IBM-Bluemix/logistics-wizard/issues/197

--- server/config.py
+++
@@ -1,8 +0,0 @@
-from os import environ as env
-
-
-class Config(object):
-
-    ENVIRONMENT = env.get('LOGISTICS_WIZARD_ENV', 'DEV').upper()
-    SD_STATUS = env.get('SD_STATUS', 'OFF')
-    SECRET = env.get('SECRET', 'secret')
